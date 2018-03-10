d577295ae657540b10ceee427e5aa1ec889906a6
0326 <winniringy@gmail.com>
2018-03-10 15:36:32 +0800

    add ICG

---
+++ article/L1/resources.md
@@ -0,0 +1,8 @@
+# 学习资源
+
+[WebGL 技术储备指南](http://taobaofed.org/blog/2015/12/21/webgl-handbook/)
+>淘宝 FED 团队的 WebGL 技术储备指南。
+
+[Hi,WebGL中文教程](http://www.hiwebgl.com/?p=42)
+>LearingWebGL.com的作者Giles Thomas提供了一系列通俗易懂、由浅入深的教程的中文版。
+
---
+++ demo/webgl/demo1.glsl
@@ -0,0 +1,11 @@
+// 顶点着色器
+
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
